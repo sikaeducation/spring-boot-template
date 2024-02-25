b5ebedb0d595044e7a5220a1052e77f1a455ce29
Kyle Coberly <kyle.coberly@gmail.com>
2024-02-24 17:32:36 -0700

    Initial commit

---
+++ src/main/java/com/sikaeducation/startertemplate/StarterTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.sikaeducation.startertemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarterTemplateApplication.class, args);
+	}
+
+}
